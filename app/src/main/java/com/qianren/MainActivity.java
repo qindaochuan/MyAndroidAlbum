6424a751a667d33841590730af01d9b15a5abbfc
jun zhong <>
2020-01-13 17:25:15 +0800

    no message

--- app/src/main/java/com/qianren/MainActivity.java
+++ app/src/main/java/com/qianren/MainActivity.java
@@ -1,6 +1,7 @@
 package com.qianren;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+    }
+
+    public boolean removeFragment(Fragment fragment) {
+        return true;
     }
 }
